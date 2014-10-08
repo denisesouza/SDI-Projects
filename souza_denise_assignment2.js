ecc31687ae4a817e23eb625f4d7209ca64c2ca37
Denise Souza <SWEETD@Denises-MacBook-Pro.local>
2014-10-08 16:15:28 -0400

    Completed Procedure portion of Project 2

--- souza_denise_assignment2.js
+++ souza_denise_assignment2.js
@@ -1,1 +1,46 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Denise Souza
+//SDI 1410
+//Assignment 2 / Project 2
+
+
+
+//Start
+
+  //Global Variables
+
+  var dogNames= [ "Payton", "Logan", "Brock", "Brucie", "Greyson"];
+  var minutesToFeed = [ 15, 10, 10, 10, 15];
+  var needsWater = true;
+  var needsMoreFood = true;
+  var dogsOutside;
+
+
+
+      //Procedure
+      //If dogs need to go outside, yes or no? If yes, let them out. If no
+      //they are ready to eat if there is enough food.
+
+      dogOutside = prompt("Do the dogs need to go outside?", " Yes or No?");
+
+        if (dogOutside === "Yes") {
+              console.log("Let the dogs outside before feeding them.");
+
+          } else {
+              console.log(dogNames + " are ready to eat if there is enough food.");
+
+
+        };
+
+      //End of Procedure
+
+
+
+
+
+      //Boolean Function
+
+
+
+      //End of Boolean Function
