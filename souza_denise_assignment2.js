2072c2703ea7bc82fc9f97343ce04fda43b2d6de
Denise Souza <SWEETD@Denises-MacBook-Pro.local>
2014-10-09 21:30:32 -0400

    Lots of changes to include story line

--- souza_denise_assignment2.js
+++ souza_denise_assignment2.js
@@ -1,99 +1,134 @@
-//alert("JavaScript works!");
 
 //Denise Souza
 //SDI 1410
 //Assignment 2 / Project 2
 
 
-
 //Start
-
-  //Global Variables
-
-  var dogNames= [ "Payton", " Logan", " Brock", " Brucie", " Greyson"];
-  var minutesToFeed = [ 15, 10, 10, 10, 15];
-  var needsWater = true;
-  var needsMoreFood = true;
-  var dogsOutside;
-
+//Making Portuguese Shrimp Mozambique
+var portugueseSauce = ["goya powder", "butter", "garlic", "beer", "hot sauce"];
+var readyToCook = true;
+var sauceBoiling = true; //
+var boilingMins = 10;
+var grandmaRecipe = "Grandma Silva\Õs Recipe";
+var email = "Mom sent email";
 
 
-      //Procedure
-      //If dogs need to go outside, yes or no? If yes, let them out. If no
-      //they are ready to eat if there is enough food.
+// Procedure Function 
 
-  dogOutside = prompt("Do the dogs need to go outside?", " Yes or No?");
-
-     if (dogOutside === "Yes") {
-              console.log("Let the dogs outside before feeding them.");
-
-          } else {
-              console.log(dogNames + " are ready to eat if there is enough food.");
-
-      };
-
-      //End Procedure
+var readyCheck = function(status) {
+    if (status === true) {
+        console.log("We are ready to cook some mozambique!");
+    } else {
+        console.log("We are hungry!");
+    }
+};
 
 
+// Boolean Function 
+var cookRice = function(waterBoil, minutesBoiling) {
+    if ((waterBoil || !waterBoil) && (minutesBoiling < 8)) {
+        console.log("The rice is still cooking. Keep stirring!");
+        return false;
+    } else if (!waterBoil && (minutesBoiling > 8)) {
+        //console.log("Well how have you been cooking rice without the water boiling?");
+        return false;
+    } else {
+        //console.log("The rice is finished!");
+        return true;
+    }
+};
 
-      //Boolean Function
-      //Do the dogs need water? True or False? Do dogs have enough food for them
-      //to eat? True or False?
-      //If true feed them if not true need to
-      //go buy more food. Return with food.
 
-  needsWater = confirm("Do the dogs need water? If Yes, click OK");
-	     if (needsWater === true) {
-		        console.log ("Fill the dogs water bowl with fresh water.");
+// Number Function 
+var cookShrimp = function(cookingTime) {
+    var finishTime = 15;
+    var timeLeft = finishTime - cookingTime;
+    while (cookingTime <= finishTime) {
+        if (cookingTime == 1) {
+            console.log(cookingTime + " minute down. Stir the shrimp!" + " Time remaining:" + timeLeft + " minutes.");
+            cookingTime += 5;
+        } else {
+            console.log(cookingTime + " minutes down. Stir the shrimp!" + " Time remaining:" + timeLeft + " minutes.");
+            cookingTime += 5;
+        }
+    }
+    console.log("The shrimp is finished!");
+    return timeLeft; // return
+};
+
+
+
+// NumberFunction
+var cookSauce = function(portionSize, ingredients) {
+    var sauceCookTime = 10;
+    if (portionSize < 2) {
+        sauceCookTime = 10;
+    } else {
+        sauceCookTime = sauceCookTime * portionSize / 2;
+    }
+
+
+    for (var i = 0; i < portugueseSauce.length; i++) {
+        if (portionSize < 2) {
+            console.log("Put the " + ingredients[i] + " in the pan!");
+        } else {
+            console.log("Put " + portionSize + "x the " + ingredients[i] + " in the pan!");
         }
 
-  needsMoreFood = confirm("Do we need to go buy more dog food? If Yes, click OK");
-	     if (needsMoreFood === true){
-            console.log ("Need to head to the store to get more food");
-          } else {
-            console.log("Feed " + dogNames);
 
+    }
+    console.log("Everything is in the pan. Cook the sauce for " + sauceCookTime + " minutes.");
+    while (sauceCookTime > 0) {
+        if (sauceCookTime == 1) {
+            console.log("Sauce is almost done! Only " + sauceCookTime + " minute left.");
+        } else {
+            console.log("Sauce is almost! Only " + sauceCookTime + " minutes left.");
         }
+        sauceCookTime -= 5;
+    }
+    console.log("Sauce is finished!");
+    return ingredients;
+};
 
-    var priceOfFood = 55;
-    while (priceOfFood > 0) {
-          console.log("We have " + priceOfFood + " dollars left to spend on dog food");
-          priceOfFood = priceOfFood - 10;
-
-    };
-    console.log("We are out of money for food.")
-		//howMuchDogFood = prompt("How many cups of " + dryDogFood + " do I have?", "1 or 3?");
-
-
-
-
+// String Function
+var recipeLookup = function(source, email) {
+    var recipe = "I found the recipe from " + source + " using the " + email + ".";
+    return recipe;
+};
 
 
-      //End of Boolean Function
+//Main Code
 
-      //Number Function
-      //The amount of time it takes to feed each of the dogs.
-
-
-    //  var dogNames= [ "Payton", "Logan", "Brock", "Brucie", "Greyson"];
-    //  var minutesToFeed = [ 15, 10, 10, 10, 15];
+status = prompt("Are we ready to cook?", "Yes");  //prompt procedure
 
 
-    //  for(var dogNumber=0; dogNames < dogNames.length; dogNumber++) {
-        //  var dogNames = dogNames [dogNumber],
-            //  minutesPerDog = minutesToFeed [dogNumber];
-            //  console.log("Feeding " + dogNames + " will take me " + minutesPerDog +
-            //  "minutes.");
-
-
-        //for(var minutes=0; minutes < minutesPerDog; minutes && 1) {
-          //  var minutesItTook = minutesPerDog - minutes;
-          //  console.log ("It took " + minutesItTook + minutesPerDog + "to feed " +
-          //  dogName + ".");
-
-    //  }
+readyCheck(readyToCook); //call Procedure
 
 
 
+var cookedRice = cookRice(sauceBoiling, boilingMins); 
+cookedRice = confirm("Has the rice finished cooking?, If Yes, click OK"); //confirm boolean
+if (cookedRice === true) {
+    console.log("Yaaay, the rice is done cooking.");
+} else {
+    console.log("Ouch this rice is hard and not cooked");
+}
 
-//  }; //What dog I am feeding
+timeLeft = parseInt(prompt("How much time left?"))
+var timeLeft = cookShrimp(2); // Call cookShrimp Number function
+
+recipeLookup(grandmaRecipe, email); // Call recipeLookup String function
+
+
+var sauce = cookSauce(3, portugueseSauce); // Call cookSauce 
+
+
+console.log("How much time is left on the shrimp?");
+console.log("The sauce has " + timeLeft + " minutes left.");
+
+console.log("Great recipe where did you find it?");
+console.log(grandmaRecipe);
+
+console.log("You want the secret ingredients, here you go?");
+console.log("They are: " + sauce.toString() + ".");
