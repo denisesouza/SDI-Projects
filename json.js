c4188ea7f5f010908099a061ea4ac89ffe8a40ac
Denise Souza <SWEETD@Denises-MacBook-Pro.local>
2014-10-15 23:34:08 -0400

    buit and added json data

---
+++ json.js
@@ -0,0 +1,51 @@
+//Denise Souza
+//SDI 1410
+//Assignment 3 / Project 3
+//JSON DATA
+
+var playerInfo = {
+    "players": [{
+            "name": "R. Wilson",
+            "position": "QB",
+            "team": "Seattle",
+            "starter": true
+        }, {
+            "name": "E. Lacy",
+            "position": "RB",
+            "team": "Green bay",
+            "starter": true
+        }, {
+            "name": "A. Ellington",
+            "position": "RB",
+            "team": "Arizona",
+            "starter": true
+        }, {
+            "name": "J. Nelson",
+            "position": "WR
+            "team": "Green bay",
+            "starter": true
+        }, {
+            "name": "P. Garcon",
+            "position": "WR",
+            "team": "Washington",
+            "starter": true
+        }, {
+            "name": "H. Miller",
+            "position": "TE,
+            "team": "Pitt",
+            "starter": true
+        }, {
+            "name": "K. Wright",
+            "position": "WR",
+            "team": "Tennessee",
+            "starter": true
+        }, {
+            "name": "D. Bailey",
+            "position": "K",
+            "team": "Dallas",
+            "starter": true
+        }
+
+
+    ]
+};
