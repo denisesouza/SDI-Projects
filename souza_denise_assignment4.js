bc0c12dbf531a31fc1d71769f03c1520b545ef1b
Denise Souza <SWEETD@Denises-MacBook-Pro.local>
2014-10-23 18:43:04 -0400

    corrected syntax of code and made it look pretty

--- souza_denise_assignment4.js
+++ souza_denise_assignment4.js
@@ -2,11 +2,7 @@
 //SDI 1410
 //October, 23 2014
 //Assignment 4
-
-
-
 //global variables
-
 var stringEmailReturn;
 var stringTitleUpperReturn;
 var stringNumberReturn;
@@ -27,22 +23,22 @@ var numberDecimalReturn;
 //returns boolean
 //#1
 
-function checkNumber(p){
-            var x = p.split(''); // splits the phone number into array
-           
-            for (var i=0; i < x.length; i++){
-                // Skip the "-" since we know they'll fail
-                if (i == 3 || i == 7) continue;
-           
-                // Check each digit, failing on the first non-numeric character
-                if(isNaN(x[i]))
-                    return false;
-            }
-            return true;
-        }
-       
-        var phone = '508-813-6453'  
-        alert(checkNumber(phone));
+function checkNumber(p) {
+    var x = p.split(''); // splits the phone number into array
+
+    for (var i = 0; i < x.length; i++) {
+        // Skip the "-" since we know they'll fail
+        if (i == 3 || i == 7) continue;
+
+        // Check each digit, failing on the first non-numeric character
+        if (isNaN(x[i]))
+            return false;
+    }
+    return true;
+}
+
+var phone = '508-813-6453'
+alert(checkNumber(phone));
 
 
 
@@ -52,35 +48,31 @@ function checkNumber(p){
 //#2
 
 var checkEmail = function(text) {
-	var atposition=text.indexOf("@");
-	var dotposition=text.lastIndexOf(".");
-	
-if(atposition < 1 || dotposition <atposition+2 || dotposition+2>text.length){
-		return false; //alert("This does not match email pattern");
-		} else {
-		return true;
-		};
-		};
+    var atposition = text.indexOf("@");
+    var dotposition = text.lastIndexOf(".");
+
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 > text.length) {
+        return false; //alert("This does not match email pattern");
+    } else {
+        return true;
+    };
+};
 
 
 // Changes the first letter of the string to uppercaseCapitalize the first character of each word in a string
 // Returns modified string
 //#3
 
-var checkTitleUpper = function (str) {
-var split = str.split(" ");
-var result = "";
-for (var i = 0, j = split.length; i < j; i++) {
-var spNew = split[i].replace(split[i].charAt(0),(split[i].charAt(0)).toUpperCase());
-result = result.concat(spNew + " ");
-}
-return result;
+var checkTitleUpper = function(str) {
+    var split = str.split(" ");
+    var result = "";
+    for (var i = 0, j = split.length; i < j; i++) {
+        var spNew = split[i].replace(split[i].charAt(0), (split[i].charAt(0)).toUpperCase());
+        result = result.concat(spNew + " ");
+    }
+    return result;
 
 };
-
-
-
-
 
 
 
@@ -93,9 +85,9 @@ return result;
 // Format a number in decimal form to a specific number of decimals places
 //returns rounded number up 2 decimal places
 //#4
-var formatDecimal = function (num,afterDecimal) {
-        return Number(num.toFixed(afterDecimal));
-    };
+var formatDecimal = function(num, afterDecimal) {
+    return Number(num.toFixed(afterDecimal));
+};
 
 
 //number of hours or days between two dates
@@ -103,10 +95,10 @@ var formatDecimal = function (num,afterDecimal) {
 //#5
 
 
-var numberDays = function(date1String, date2String){
-var day1 = new Date(date1String);
-var day2 = new Date(date2String);
-return (day2-day1)/(1000*3600*24);
+var numberDays = function(date1String, date2String) {
+    var day1 = new Date(date1String);
+    var day2 = new Date(date2String);
+    return (day2 - day1) / (1000 * 3600 * 24);
 };
 
 
@@ -115,31 +107,20 @@ return (day2-day1)/(1000*3600*24);
 //returning the value as an actual number
 //#6
 
- var checkNumber = function (num) {
-return Number(num);
-    };
-
-
-
-
-
-
-
-
-//CHECK ARRAY FUNCTIONS
-
+var checkNumber = function(num) {
+    return Number(num);
+};
 
 
 
 
 //main code
-stringEmailReturn = checkEmail("dot@dot.edu");  //MAKEUP: Assignment 3 Coding Logic & Syntax & Assignment 1 Output of initial Boolean
+stringEmailReturn = checkEmail("dot@dot.edu"); //MAKEUP: Assignment 3 Coding Logic & Syntax & Assignment 1 Output of initial Boolean
 console.log(stringEmailReturn);
-numberDecimalReturn=formatDecimal(1.02914,2);
+numberDecimalReturn = formatDecimal(1.02914, 2);
 console.log(numberDecimalReturn);
 console.log("I have been on this earth for " + numberDays('1980-11-02', '2014-10-23') + " days!");
 stringTitleUpperReturn = checkTitleUpper("this is me hoping it works");
 console.log(stringTitleUpperReturn);
 stringNumberReturn = checkNumber("42")
 console.log(stringNumberReturn);
-
